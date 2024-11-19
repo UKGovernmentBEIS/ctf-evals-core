0b21d6dd2b66b6c7d4beb5ffdc81ac0a920bf8d3
Will.Payne <Will.Payne@dsit.gov.uk>
2024-11-19 14:24:49 +0000

    Clean up docker.py

--- src/ctf_evals_core/_util/docker.py
+++ src/ctf_evals_core/_util/docker.py
@@ -10,6 +10,10 @@ from pydantic import field_validator
 
 
 class ImagePlan(pydantic.BaseModel):
+    """Base class for image plans. Subclasses should implement get_image_name.
+
+    The context field should be a path to the directory containing the Dockerfile.
+    """
     context: Path
 
     @field_validator("context")
@@ -58,6 +62,12 @@ class ImagePlan(pydantic.BaseModel):
 
 
 class ChallengeImagePlan(ImagePlan):
+    """Image plan for challenge images.
+
+    The context should be a path to the directory containing the Dockerfile, which
+    should be of the form challenge_folder/images/service_folder. where the challenge
+    folder contains a challenge.yaml file.
+    """
     @field_validator("context")
     @classmethod
     def validate_context(cls, v):
@@ -90,6 +100,12 @@ class ChallengeImagePlan(ImagePlan):
 
 
 class CommonImagePlan(ImagePlan):
+    """Image plan for common images.
+
+    The context should be a path to the directory containing the Dockerfile, which
+    should be of the form images/some_image_folder.
+    """
+
     @field_validator("context")
     @classmethod
     def validate_context(cls, v):
@@ -114,6 +130,12 @@ class CommonImagePlan(ImagePlan):
 
 
 class EvalsCoreImagePlan(ImagePlan):
+    """Image plan for the ctf_evals_core image.
+
+    The context should be a path to the directory containing the Dockerfile, which
+    should be of the form ctf_evals_core/images/some_image_folder.
+    """
+
     @field_validator("context")
     @classmethod
     def validate_context(cls, v):
@@ -141,9 +163,141 @@ class EvalsCoreImagePlan(ImagePlan):
         assert re.match(regex, image_name), f"Invalid image name: {image_name}"
         return image_name
 
+def get_images(root_dir: Path | None = None) -> list[ImagePlan]:
+    """Discovers all images in the given root directory.
 
-# Discovers challenge image files in cwd/challenges, and uses a specific map from image
-# path to image name
+    Args:
+        root_dir (Path, optional): The root directory to search for images. Defaults to
+            None. if None, the current working directory is used.
+    """
+    root_dir = root_dir or Path.cwd()
+    challenge_images = _discover_challenge_dockerfiles(root_dir=root_dir)
+    common_images = _discover_common_images(root_dir=root_dir)
+    evals_core_images = _discover_evals_core_images()
+    all_images = challenge_images + common_images + evals_core_images
+    return cast(list[ImagePlan], all_images)
+
+
+class Registry(pydantic.BaseModel):
+    """Represents an ECR registry.
+
+    Used to push images to the registry and keep track of the subdomain in which to
+    push images.
+    """
+    # Id of the registry in the form of 123456789012
+    registry_id: str
+    # subdomain of the registry of the form cyber-ctf
+    subdomain: str
+    # region of the registry
+    region: str
+    # prefix for images that aren't part of core
+    challenge_prefix: str
+
+    def registry(self):
+        """Returns the full registry name."""
+        return f"{self.registry_id}.dkr.ecr.{self.region}.amazonaws.com"
+
+    def login(self):
+        """Logs in to aws ecr."""
+        ps = subprocess.Popen(
+            ("aws", "ecr", "get-login-password", "--region", self.region),
+            stdout=subprocess.PIPE,
+        )
+        try:
+            _ = subprocess.check_output(
+                (
+                    "docker",
+                    "login",
+                    "--username",
+                    "AWS",
+                    "--password-stdin",
+                    self.registry(),
+                ),
+                stdin=ps.stdout,
+            )
+            returncode = ps.wait()
+            if returncode != 0:
+                print(f"Failed to login to {self.registry()}")
+                return False
+            return True
+        except subprocess.CalledProcessError:
+            print(f"Failed to login to {self.registry()}")
+            return False
+
+    def get_image_repository(self, image: ImagePlan):
+        """Returns the repository name for the image."""
+        if isinstance(image, EvalsCoreImagePlan):
+            return f"{self.subdomain}/{image.get_image_name()}"
+        return f"{self.subdomain}/{self.challenge_prefix}/{image.get_image_name()}"
+
+    def _maybe_create_repository(self, image: ImagePlan):
+        tags = self.get_image_tags(image)
+        if len(tags) > 0:
+            return
+        returncode = subprocess.check_call(
+            [
+                "aws",
+                "ecr",
+                "create-repository",
+                "--repository-name",
+                self.get_image_repository(image),
+                "--image-scanning-configuration",
+                "scanOnPush=true",
+                "--image-tag-mutability",
+                "IMMUTABLE",
+            ],
+            stdout=subprocess.DEVNULL,
+        )
+        if returncode != 0:
+            print(f"Failed to create repository {
+                  self.get_image_repository(image)}")
+            return False
+        return True
+
+    def _get_ecr_client(self):
+        return boto3.client("ecr", region_name=self.region)
+
+    def get_image_tags(self, image: ImagePlan):
+        """Get the tags for the given image."""
+        # Create an ECR client
+        client = self._get_ecr_client()
+        try:
+            response = client.list_images(
+                repositoryName=self.get_image_repository(image)
+            )
+            tags = [image["imageTag"] for image in response["imageIds"]]
+            return tags
+        except client.exceptions.RepositoryNotFoundException:
+            return []
+        except client.exceptions.ClientError:
+            return []
+
+    def _check_tag_exists(self, image: ImagePlan, tag: str):
+        return tag in self.get_image_tags(image)
+
+    def _get_full_image_name(self, image: ImagePlan):
+        return f"{self.registry()}/{self.get_image_repository(image)}"
+
+    def push_image(self, image: ImagePlan, tag: str):
+        """Push an ImagePlan to the registry with the given tag."""
+        # 1. Checkif the tag already exists (tags are immutable)
+        if self._check_tag_exists(image, tag):
+            raise ValueError(f"Tag {tag} already exists for image {image}")
+
+        self._maybe_create_repository(image)
+        # 2. Build image with local naming then tag with full ecr name
+        image.build_image()
+        image_name = f"{self._get_full_image_name(image)}:{tag}"
+        image.tag(image_name)
+        # 3. Push the image
+        returncode = subprocess.check_call(
+            ["docker", "push", image_name],
+        )
+        if returncode != 0:
+            print(f"Failed to push image {tag}")
+            return False
+        return True
+
 def _discover_challenge_dockerfiles(root_dir: Path) -> list[ChallengeImagePlan]:
     results = glob(f"{root_dir}/challenges/**/Dockerfile", recursive=True)
     # Parent because docker expects the folder containing the Dockerfile
@@ -180,124 +334,3 @@ def _discover_evals_core_images() -> list[EvalsCoreImagePlan]:
         EvalsCoreImagePlan.from_dockerfile_path(result) for result in results
     ]
     return image_plans
-
-
-def get_images(root_dir: Path | None = None) -> list[ImagePlan]:
-    root_dir = root_dir or Path.cwd()
-    challenge_images = _discover_challenge_dockerfiles(root_dir=root_dir)
-    common_images = _discover_common_images(root_dir=root_dir)
-    evals_core_images = _discover_evals_core_images()
-    all_images = challenge_images + common_images + evals_core_images
-    return cast(list[ImagePlan], all_images)
-
-
-class Registry(pydantic.BaseModel):
-    # Id of the registry in the form of 123456789012
-    registry_id: str
-    # subdomain of the registry of the form cyber-ctf
-    subdomain: str
-    # region of the registry
-    region: str
-    # prefix for images that aren't part of core
-    challenge_prefix: str
-
-    def registry(self):
-        return f"{self.registry_id}.dkr.ecr.{self.region}.amazonaws.com"
-
-    def login(self):
-        ps = subprocess.Popen(
-            ("aws", "ecr", "get-login-password", "--region", self.region),
-            stdout=subprocess.PIPE,
-        )
-        try:
-            _ = subprocess.check_output(
-                (
-                    "docker",
-                    "login",
-                    "--username",
-                    "AWS",
-                    "--password-stdin",
-                    self.registry(),
-                ),
-                stdin=ps.stdout,
-            )
-            returncode = ps.wait()
-            if returncode != 0:
-                print(f"Failed to login to {self.registry()}")
-                return False
-            return True
-        except subprocess.CalledProcessError:
-            print(f"Failed to login to {self.registry()}")
-            return False
-
-    def get_image_repository(self, image: ImagePlan):
-        """Returns the repository name for the image."""
-        if isinstance(image, EvalsCoreImagePlan):
-            return f"{self.subdomain}/{image.get_image_name()}"
-        return f"{self.subdomain}/{self.challenge_prefix}/{image.get_image_name()}"
-
-    def maybe_create_repository(self, image: ImagePlan):
-        tags = self.get_image_tags(image)
-        if len(tags) > 0:
-            return
-        returncode = subprocess.check_call(
-            [
-                "aws",
-                "ecr",
-                "create-repository",
-                "--repository-name",
-                self.get_image_repository(image),
-                "--image-scanning-configuration",
-                "scanOnPush=true",
-                "--image-tag-mutability",
-                "IMMUTABLE",
-            ],
-            stdout=subprocess.DEVNULL,
-        )
-        if returncode != 0:
-            print(f"Failed to create repository {
-                  self.get_image_repository(image)}")
-            return False
-        return True
-
-    def get_ecr_client(self):
-        return boto3.client("ecr", region_name=self.region)
-
-    def get_image_tags(self, image: ImagePlan):
-        # Create an ECR client
-        client = self.get_ecr_client()
-        try:
-            response = client.list_images(
-                repositoryName=self.get_image_repository(image)
-            )
-            tags = [image["imageTag"] for image in response["imageIds"]]
-            return tags
-        except client.exceptions.RepositoryNotFoundException:
-            return []
-        except client.exceptions.ClientError:
-            return []
-
-    def check_tag_exists(self, image: ImagePlan, tag: str):
-        return tag in self.get_image_tags(image)
-
-    def get_full_image_name(self, image: ImagePlan):
-        return f"{self.registry()}/{self.get_image_repository(image)}"
-
-    def push_image(self, image: ImagePlan, tag: str):
-        # 1. Checkif the tag already exists (tags are immutable)
-        if self.check_tag_exists(image, tag):
-            raise ValueError(f"Tag {tag} already exists for image {image}")
-
-        self.maybe_create_repository(image)
-        # 2. Build image with local naming then tag with full ecr name
-        image.build_image()
-        image_name = f"{self.get_full_image_name(image)}:{tag}"
-        image.tag(image_name)
-        # 3. Push the image
-        returncode = subprocess.check_call(
-            ["docker", "push", image_name],
-        )
-        if returncode != 0:
-            print(f"Failed to push image {tag}")
-            return False
-        return True
