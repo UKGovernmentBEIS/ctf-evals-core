0f177653323c4dcac229472636fe4cc9f6185c05
Sam.Deverett <Sam.Deverett@dsit.gov.uk>
2025-01-22 17:13:59 +0000

    Add a basic test script.

---
+++ example/test_multiple_tasks.py
@@ -0,0 +1,9 @@
+from ctf_evals_core.task import create_ctf_tasks
+from inspect_ai import eval_set
+
+challenges = ""
+variants = "solution"
+single_task = False
+
+tasks = create_ctf_tasks(challenges=challenges, variants=variants, single_task=single_task)
+eval_set(tasks, log_dir="logs", model="openai/gpt-4o", max_tasks=5)
