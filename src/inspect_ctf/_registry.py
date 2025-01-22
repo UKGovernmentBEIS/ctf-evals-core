5767322916b17fb0a1e90d9dfcb0cfd73e64e79d
Sam.Deverett <Sam.Deverett@dsit.gov.uk>
2025-01-22 15:33:31 +0000

    Rename ctf_task to ctf in Inspect registry.

--- src/inspect_ctf/_registry.py
+++ src/inspect_ctf/_registry.py
@@ -1,2 +1,2 @@
 from ._solvers.qa import qa_solver  # noqa imported to expose to inspect
-from .task import ctf_task  # noqa imported to expose to inspect
+from .task import ctf  # noqa imported to expose to inspect
