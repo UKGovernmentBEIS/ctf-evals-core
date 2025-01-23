45cfb25dd033db202ade89b4434c1be2a0d86a9a
Will.Payne <Will.Payne@dsit.gov.uk>
2025-01-23 12:09:29 +0000

    Ruff format

--- example/test_multiple_tasks.py
+++ example/test_multiple_tasks.py
@@ -2,7 +2,7 @@ from ctf_evals_core.task import create_ctf_tasks
 from inspect_ai import eval_set
 
 challenges = ""
-variants = ["solution","minimal"]
+variants = ["solution", "minimal"]
 metadata_filters = None
 # metadata_filters=["filter_var=True"] # should only get the hello world task
 single_task = False
@@ -11,6 +11,6 @@ tasks = create_ctf_tasks(
     challenges=challenges,
     variants=variants,
     single_task=single_task,
-    metadata_filters=metadata_filters
+    metadata_filters=metadata_filters,
 )
 eval_set(tasks, log_dir="logs", model="openai/gpt-4o", max_tasks=5)
